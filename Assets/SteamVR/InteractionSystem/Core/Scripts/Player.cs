347705aa458ce8510ff9352cd30b64b5014f46d2
Kyle <kyle_michiels@hotmail.com>
2020-02-01 22:32:57 +0100

    DemoScene

--- Assets/SteamVR/InteractionSystem/Core/Scripts/Player.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/Player.cs
@@ -279,7 +279,7 @@ namespace Valve.VR.InteractionSystem
 			}
 
             yield return new WaitForSeconds(0.1f);
-            transform.localScale = new Vector3(7.5f, 7.5f, 7.5f);
+            transform.localScale = new Vector3(50.0f, 50.0f, 50.0f);
         }
 
         protected virtual void Update()
