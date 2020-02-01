fad58b467f87127c96e1e3ebe6831285401ec7c7
Jens Petter <j.petter111@gmail.com>
2020-02-01 17:40:05 +0100

    [Added] A button multi controller support.

--- Assets/Jens/Scripts/PlayerInteraction.cs
+++ Assets/Jens/Scripts/PlayerInteraction.cs
@@ -19,13 +19,13 @@ public class PlayerInteraction : MonoBehaviour
     }
 
     void Update() {
-        if (Input.GetButtonDown("TriggerJoystick1")) {
+        if (ControllerManager.GetAButtonFromPlayer(GetComponent<MoveTo>().m_PlayerID)) {
             if (m_CanInteract) {
                 Interact();
             }
         }
 
-        if (ControllerManager.GetTriggerFromPlayer(GetComponent<MoveTo>().m_PlayerID) > 0.2f) {
+        if (ControllerManager.GetAButtonDownFromPlayer(GetComponent<MoveTo>().m_PlayerID)) {
             GetComponent<Animator>().SetBool("bounce", true);
         }
         else {
@@ -35,7 +35,7 @@ public class PlayerInteraction : MonoBehaviour
         if (m_IsDestroying) {
             m_DestroyCounter = m_DestroyCounter + Time.deltaTime;
 
-            if (Input.GetButtonUp("TriggerJoystick1")) {
+            if (ControllerManager.GetAButtonUpFromPlayer(GetComponent<MoveTo>().m_PlayerID)) {
                 m_IsDestroying = false;
                 m_CanInteract = true;
             }
