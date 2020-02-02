3108619bb937ae5023a7c5a5b7c42ac8a97d0e53
SebastianBinder-dev <SebastianBinder-dev@users.noreply.github.com>
2020-02-02 01:25:50 +0100

    UI Updates
    UI Updates

---
+++ Assets/UI Game/ProgressBar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+#if UNITY_EDITOR
+   [MenuItem("GameObject/UI Game/LinearProgressBar")]
+    public static void AddLinearProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("GameObject/UI Game/LinearProgressBar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+
+    [MenuItem("GameObject/UI Game/RadialProgressBar")]
+    public static void AddRadialProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("GameObject/UI Game/RadialProgressBar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+
+#endif
+
+    public int minimum;
+    public int maximum;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Text progressText;
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = FaithController.m_Faith / 100;
+        mask.fillAmount = fillAmount;
+        fill.color = color;
+    }
+
+}
---
+++ Assets/UI Game/ScoreUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializedField] public Transform ScorePopup;
+
+    void Start()
+    {
+        Instantiate
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
